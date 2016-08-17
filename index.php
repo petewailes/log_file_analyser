c8a8b775fc0e661c2401779da590f8c07564b21d
Pete Wailes <pete@wail.es>
2016-08-17 12:45:23 +0100

    refactored to tidy up code generation and layout

--- index.php
+++ index.php
@@ -55,14 +55,11 @@ $parser->setRegex("/^(?P<ipAddress>\S+) (\S+) (\S+) \[(?P<date>[^:]+):(?P<time>\
 $log_lines = file('2016-08-15-access.log', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); // testing mode
 // $log_lines = file(date('Y-m-d').'-access.log', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); // prod mode
 
-$total_count = 0;
-
-$googlebot_crawl = array('codes' => array(), 'exts' => array(), 'dates' => array(), 'regexes' => array());
+$dates = $crawl = array();
 
 // change these to anything you want to particularly note in your crawls
 // the first part gives a key used for reporting the area, the second is the regex to match with agains the URL path
 $regexes = array(
-	'Fly Me to the Moon' => '/^\/fly-me-to-the-moon.+/',
 	'Parameters' => '/^.+\?.+/',
 	'UTM' => '/^.+[&|\?]utm_.+/'
 );
@@ -71,59 +68,61 @@ foreach ($log_lines as $line) {
 	$entry = $parser->parse($line);
 
 	if (is_object($entry) && strpos($entry->requestFrom, 'Googlebot')) {
-		$total_count++;
+		if (!isset($dates[$entry->date]))
+			$dates[$entry->date] = 1;
+		else
+			$dates[$entry->date]++;
 
 		$cleaned_path = (strpos($entry->urlPath, "?") !== FALSE) ? substr($entry->urlPath, 0, strpos($entry->urlPath, "?")) : $entry->urlPath;
 
 		$parts = pathinfo($cleaned_path);
 		$ext = (isset($parts['extension'])) ? $parts['extension'] : 'html';
 
-		if (isset($googlebot_crawl['codes'][$entry->responseCode]['requests']))
-			$googlebot_crawl['codes'][$entry->responseCode]['requests']++;
-		else
-			$googlebot_crawl['codes'][$entry->responseCode]['requests'] = 1;
+		if (!isset($crawl[$entry->urlPath])) {
+			$crawl[$entry->urlPath] = array(
+				'requests' => 1,
+				'ext' => $ext,
+				'codes' => array($entry->responseCode => 1),
+				'dates' => array($entry->date),
+				'regexes' => array()
+			);
 
-		if (isset($googlebot_crawl['codes'][$entry->responseCode]['urlPaths'][$entry->urlPath]))
-			$googlebot_crawl['codes'][$entry->responseCode]['urlPaths'][$entry->urlPath]++;
-		else
-			$googlebot_crawl['codes'][$entry->responseCode]['urlPaths'][$entry->urlPath] = 1;
-
-		if (isset($googlebot_crawl['exts'][$ext]['requests']))
-			$googlebot_crawl['exts'][$ext]['requests']++;
-		else
-			$googlebot_crawl['exts'][$ext]['requests'] = 1;
-
-		if (isset($googlebot_crawl['exts'][$ext]['urlPaths'][$entry->urlPath]))
-			$googlebot_crawl['exts'][$ext]['urlPaths'][$entry->urlPath]++;
-		else
-			$googlebot_crawl['exts'][$ext]['urlPaths'][$entry->urlPath] = 1;
-
-		if (isset($googlebot_crawl['dates'][$entry->date]))
-			$googlebot_crawl['dates'][$entry->date]++;
-		else
-			$googlebot_crawl['dates'][$entry->date] = 1;
-
-		foreach ($regexes as $name => $regex) {
-			if (preg_match($regex, $entry->urlPath, $matches)) {
-				if (isset($googlebot_crawl['regexes'][$name]['requests']))
-					$googlebot_crawl['regexes'][$name]['requests']++;
-				else
-					$googlebot_crawl['regexes'][$name]['requests'] = 1;
-
-				if (isset($googlebot_crawl['regexes'][$name]['urlPaths'][$entry->urlPath]))
-					$googlebot_crawl['regexes'][$name]['urlPaths'][$entry->urlPath]++;
-				else
-					$googlebot_crawl['regexes'][$name]['urlPaths'][$entry->urlPath] = 1;
+			foreach ($regexes as $title => $regex) {
+				if (preg_match($regex, $entry->urlPath, $matches)) {
+					if (!isset($crawl[$entry->urlPath]['regexes'][$title]))
+						$crawl[$entry->urlPath]['regexes'][$title] = 1;
+					else
+						$crawl[$entry->urlPath]['regexes'][$title]++;
+				}
 			}
+		}
+		else {
+			$crawl[$entry->urlPath]['requests']++;
+
+			if (!isset($crawl[$entry->urlPath]['codes'][$entry->responseCode]))
+				$crawl[$entry->urlPath]['codes'][$entry->responseCode] = 1;
+			else
+				$crawl[$entry->urlPath]['codes'][$entry->responseCode]++;
+
+			if (!in_array($entry->date, $crawl[$entry->urlPath]['dates']))
+				$crawl[$entry->urlPath]['dates'][] = $entry->date;
 		}
 	}
 }
 
-foreach (array('codes', 'exts', 'regexes') as $type) {
-	foreach ($googlebot_crawl[$type] as $subtype => $code_data) {
-		arsort($googlebot_crawl[$type][$subtype]['urlPaths']);
-	}
+foreach ($crawl as $path => $row) {
+	arsort($crawl[$path]['codes']);
 }
+
+// sort everything by requests
+uasort($crawl, function ($a, $b) {
+	if ($a['requests'] > $b['requests'])
+		return -1;
+	elseif ($a['requests'] < $b['requests'])
+		return 1;
+	else
+		return 0;
+});
 ?>
 <!DOCTYPE html>
 <html>
@@ -132,6 +131,34 @@ foreach (array('codes', 'exts', 'regexes') as $type) {
 	<link rel="stylesheet" type="text/css" href="minimal_css.min.css">
 </head>
 <body>
+	<nav id="navbar">
+		<ul>
+			<?php
+			foreach (array('html','css','js','png','jpg') as $ext):
+				foreach (array(200,301,404) as $status_code):
+					foreach ($crawl as $path => $row):
+						if ($row['ext'] === $ext && isset($row['codes'][$status_code])):
+			?>
+			<li><a href="#<?php echo $ext.$status_code ?>"><?php echo "$ext $status_code" ?></a></li>
+			<?php
+							continue 2;
+						endif;
+					endforeach;
+				endforeach;
+			endforeach;
+			foreach ($regexes as $title => $regex):
+				foreach ($crawl as $path => $row):
+					if (isset($row['regexes'][$title])):
+		?>
+		<li><a href="#<?php echo $title ?>"><?php echo "$title" ?></a></li>
+		<?php
+						continue 2;
+					endif;
+				endforeach;
+			endforeach;
+			?>
+		</ul>
+	</nav>
 	<div id="wrapper" class="row">
 		<div class="col col-xs-12">
 			<h1>Log File Analyser</h1>
@@ -141,16 +168,17 @@ foreach (array('codes', 'exts', 'regexes') as $type) {
 				<div id="chart"></div>
 			</div>
 			<script>
-			window["chart"] = new Highcharts.Chart({"credits":{"enabled":false},"chart":{"backgroundColor":"#fefefe","renderTo":"chart","type":"spline","spacingTop":20,"spacingBottom":20},"title":{"text":null},"colors":["#3fa9f5","#476974","#3ca1c1","#4ccbf4","#96dff6","#c9e8f6"],"legend":{"enabled":true,"margin":30},"plotOptions":{"series":{"animation":false}},"tooltip":{"backgroundColor":"#3fa9f5","borderColor":"#3fa9f5","borderRadius":0,"shadow":false,"style":{"color":"#ffffff","padding":"15px","font-size":"12px"}},"xAxis":{"tickWidth":0,"labels":{"y":20},"categories":["<?php echo implode('","', array_keys($googlebot_crawl['dates'])) ?>"]},"yAxis":{"tickWidth":0,"labels":{"y":0},"title":{"text":"Values"}},"series":[{"name":"Crawled resources per day","data":[<?php echo implode(',', $googlebot_crawl['dates']) ?>]}]});
+			window["chart"] = new Highcharts.Chart({"credits":{"enabled":false},"chart":{"backgroundColor":"#fefefe","renderTo":"chart","type":"spline","spacingTop":20,"spacingBottom":20},"title":{"text":null},"colors":["#3fa9f5","#476974","#3ca1c1","#4ccbf4","#96dff6","#c9e8f6"],"legend":{"enabled":true,"margin":30},"plotOptions":{"series":{"animation":false}},"tooltip":{"backgroundColor":"#3fa9f5","borderColor":"#3fa9f5","borderRadius":0,"shadow":false,"style":{"color":"#ffffff","padding":"15px","font-size":"12px"}},"xAxis":{"tickWidth":0,"labels":{"y":20},"categories":["<?php echo implode('","', array_keys($dates)) ?>"]},"yAxis":{"tickWidth":0,"labels":{"y":0},"title":{"text":"Values"}},"series":[{"name":"Crawled resources per day","data":[<?php echo implode(',', $dates) ?>]}]});
 			</script>
 			<p><small>Created with the <a href="https://builtvisible.com/highcharts-generator/">Highcharts Generator tool</a></small></p>
 		</div>
 		<div class="col col-xs-12">
-			<h2>Resources Crawled by Status Code</h2>
+			<h2>Resources Crawled by Status Code & Ext</h2>
 			<?php
-			foreach (array(301,404) as $status_code):
-				if (isset($googlebot_crawl['codes'][$status_code])): ?>
-			<h3><?php echo $status_code ?> URIs (<small><?php echo $googlebot_crawl['codes'][$status_code]['requests'] ?></small>)</h3>
+			foreach (array('html','css','js','png','jpg') as $ext):
+				foreach (array(200,301,404) as $status_code):
+				?>
+			<h3 id="<?php echo "$ext$status_code" ?>"><?php echo "$ext $status_code" ?> Status Code URIs</h3>
 			<div class="content">
 				<table>
 					<thead>
@@ -160,78 +188,53 @@ foreach (array('codes', 'exts', 'regexes') as $type) {
 						</tr>
 					</thead>
 					<tbody>
-					<?php foreach ($googlebot_crawl['codes'][$status_code]['urlPaths'] as $path => $count): ?>
+					<?php
+					foreach ($crawl as $path => $row):
+						if ($row['ext'] === $ext && isset($row['codes'][$status_code])): ?>
 						<tr>
 							<td><?php echo $path ?></td>
-							<td><?php echo $count ?></td>
+							<td><?php echo $row['codes'][$status_code] ?></td>
 						</tr>
-					<?php endforeach; ?>
-					</tbody>
-				</table>
-			</div>
-			<?php
-				endif;
-			endforeach;
-			?>
-		</div>
-		<div class="col col-xs-12">
-			<h2>Resources Crawled by Extension</h2>
-			<?php
-			foreach (array('css','js','png','jpg') as $ext):
-				if (isset($googlebot_crawl['exts'][$ext])): ?>
-			<h3><?php echo $ext ?> URIs (<small><?php echo $googlebot_crawl['exts'][$ext]['requests'] ?></small>)</h3>
-			<div class="content">
-				<table>
-					<thead>
-						<tr>
-							<th>Path</th>
-							<th>Count</th>
-						</tr>
-					</thead>
-					<tbody>
-					<?php foreach ($googlebot_crawl['exts'][$ext]['urlPaths'] as $path => $count): ?>
-						<tr>
-							<td><?php echo $path ?></td>
-							<td><?php echo $count ?></td>
-						</tr>
-					<?php endforeach; ?>
-					</tbody>
-				</table>
-			</div>
-			<?php
-				endif;
-			endforeach;
-			?>
-		</div>
-		<div class="col col-xs-12">
-			<h2>Resources Crawled by Regex</h2>
-			<?php
-			if (count($googlebot_crawl['regexes']) > 0):
-				foreach ($googlebot_crawl['regexes'] as $title => $dataset):
-			?>
-			<h3><?php echo $title ?> (<small><?php echo $googlebot_crawl['regexes'][$title]['requests'] ?></small>)</h3>
-			<div class="content">
-				<table>
-					<thead>
-						<tr>
-							<th>Path</th>
-							<th>Count</th>
-						</tr>
-					</thead>
-					<tbody>
-					<?php foreach ($googlebot_crawl['regexes'][$title]['urlPaths'] as $path => $count): ?>
-						<tr>
-							<td><?php echo $path ?></td>
-							<td><?php echo $count ?></td>
-						</tr>
-					<?php endforeach; ?>
+						<?php
+						endif;
+					endforeach;
+					?>
 					</tbody>
 				</table>
 			</div>
 			<?php
 				endforeach;
-			endif;
+			endforeach;
 			?>
+		</div>
+		<div class="col col-xs-12">
+			<h2>Resources Crawled by Regex</h2>
+			<?php foreach ($regexes as $title => $regex): ?>
+			<h3 id="<?php echo $title ?>"><?php echo $title ?></h3>
+			<div class="content">
+				<table>
+					<thead>
+						<tr>
+							<th>Path</th>
+							<th>Count</th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+					foreach ($crawl as $path => $row):
+						if (isset($row['regexes'][$title])): ?>
+						<tr>
+							<td><?php echo $path ?></td>
+							<td><?php echo $row['regexes'][$title] ?>(<small><?php echo $row['requests'] ?></small>)</td>
+						</tr>
+					<?php
+						endif;
+					endforeach;
+					?>
+					</tbody>
+				</table>
+			</div>
+			<?php endforeach; ?>
 		</div>
 	</div>
 </body>
